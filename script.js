13c9da527b394061172d85348ac8eeb9983a20e3
CHAN, Shu Pui <38978031+charlieop@users.noreply.github.com>
2023-07-30 21:26:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// put all the javascript code in this file
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  // you should put all the code here so that it will be executed after the page is loaded
+  init();
+});
+
+
+// you can define functions here and call them in the onload function
+function init() {
+  console.log("Hello world!");
+}
